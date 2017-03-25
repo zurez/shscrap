2ca11c5a524bf7c00db7db568364f6ab36502abc
zurez <zurez4u@gmail.com>
2017-03-25 21:08:57 +0530

    1

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 from xvfbwrapper import Xvfb
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 vdisplay = Xvfb()
 vdisplay.start()
 
 # launch stuff inside virtual display here
 print "Init"
+driver = webdriver.Chrome(executable_path="cd/chromedriver")
+print "Starting driver"
+driver.get('http://ipindiaonline.gov.in/tmrpublicsearch/frmmain.aspx')
+print "Request done"
 vdisplay.stop()
